cec707c4b86c97ccba1a400d94d61c013ddb2568
bogdan-bistriceanu <31746857+bogdan-bistriceanu@users.noreply.github.com>
2020-12-03 18:51:03 -0600

    Updates

--- 2020/Day 3/day_3.py
+++ 2020/Day 3/day_3.py
@@ -8,13 +8,8 @@ def tree_finder(char):
         return False
     
 def pos_finder(current_pos, landscape_len):
-    if current_pos % landscape_len == current_pos:
-        return current_pos
-    elif current_pos % landscape_len == 0:
-        return landscape_len - 1
-    else:
-        return current_pos % landscape_len
-
+    return current_pos % landscape_len
+    
 try: 
     with open('input.txt') as input_file:
         
@@ -27,13 +22,11 @@ try:
         for line in my_inputs[1:]:
             
             landscape = list(line)
-            print(len(landscape))
             
             if tree_finder(landscape[pos_finder(pos_cnt, len(landscape))]):
                 tree_counter = tree_counter + 1 
-            
+                
             pos_cnt = pos_cnt + 3
-            
 finally:    
     input_file.close()
     print('Trees:', tree_counter)
