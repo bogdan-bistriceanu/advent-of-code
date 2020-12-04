7e38e502846494ac48d374e210c1f726b75d8d25
bogdan-bistriceanu <31746857+bogdan-bistriceanu@users.noreply.github.com>
2020-12-03 20:22:01 -0600

    Update day_3.py

--- 2020/Day 3/day_3.py
+++ 2020/Day 3/day_3.py
@@ -1,4 +1,3 @@
-tree_counter = 0
 my_inputs = []
 
 def tree_finder(char):
@@ -9,25 +8,35 @@ def tree_finder(char):
     
 def pos_finder(current_pos, landscape_len):
     return current_pos % landscape_len
-    
-try: 
-    with open('input.txt') as input_file:
-        
-        print('file open')
-        
-        my_inputs = [line.rstrip() for line in input_file]
-        
-        pos_cnt = 3
-        
-        for line in my_inputs[1:]:
+
+def downhill(a, b):
+    tree_counter = 0
+    try: 
+        with open('input.txt') as input_file:
             
-            landscape = list(line)
+            my_inputs = [line.rstrip() for line in input_file]
             
-            if tree_finder(landscape[pos_finder(pos_cnt, len(landscape))]):
-                tree_counter = tree_counter + 1 
+            line_skip = 0
+            
+            pos_cnt = a
+            
+            for line in my_inputs[1:]:
                 
-            pos_cnt = pos_cnt + 3
-finally:    
-    input_file.close()
-    print('Trees:', tree_counter)
-    print('file closed')
+                line_skip += 1
+                
+                if line_skip == b:
+                    
+                    landscape = list(line)
+                    
+                    if tree_finder(landscape[pos_finder(pos_cnt, len(landscape))]):
+                        tree_counter = tree_counter + 1 
+                        
+                    pos_cnt = pos_cnt + a
+                    
+                    line_skip = 0
+    finally:    
+        input_file.close()
+        return(tree_counter)
+
+print("Part 1:", downhill(3,1))
+print("Part 2:", downhill(1,1)*downhill(3,1)*downhill(5,1)*downhill(7,1)*downhill(1,2))
